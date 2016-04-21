9630a9f6763d97e50d07b59ef71d14b602bf741d
JesusRuiz <ruizluna.jesussalvador@gmail.com>
2016-04-21 17:51:13 -0600

    all

---
+++ SpaApplication/Models/SpaContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SpaApplication.Models
+{
+    public class SpaContext: DbContext
+    {
+        public DbSet<Employee> Employee { get; set; }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
